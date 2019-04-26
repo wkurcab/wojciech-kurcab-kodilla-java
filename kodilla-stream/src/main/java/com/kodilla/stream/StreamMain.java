7b185f76175e68b9c849f31541961686ec4c3ff1
Wojciech Kurcab <wojciech@kurcab.com>
2019-04-26 17:20:43 +0200

    26-04

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,6 +1,7 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 
 import static jdk.nashorn.internal.objects.NativeString.toLowerCase;
 import static jdk.nashorn.internal.objects.NativeString.toUpperCase;
@@ -14,5 +15,8 @@ public class StreamMain {
         poemBeautifier.beautify("yuppi", text -> "@_### " + text + " ###_@");
         poemBeautifier.beautify("Przykładowy tekst do upiększenia", text -> "Mały tekst: " + toLowerCase(text) + " i duży tekst " + toUpperCase(text) + " koniec tekstów.");
         poemBeautifier.beautify("Inny tekst poddany Upiększaniu", text -> "& " + toLowerCase(text) + " &");
+
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
     }
 }
