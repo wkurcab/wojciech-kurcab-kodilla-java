a1047ddb6312ec1cdbdd2be4abcdd0900be0a722
Wojciech Kurcab <wojciech@kurcab.com>
2019-05-05 19:49:53 +0200

    5-05

--- kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
@@ -1,5 +1,12 @@
 package com.kodilla.stream.array;
 
+import java.util.stream.IntStream;
+
 public interface ArrayOperations {
-//    static int getAverage(int[] numbers);
+    static double getAverage(int[] numbers) {
+        return IntStream.range(0, numbers.length)
+                .map(n -> numbers[n])
+                .average()
+                .getAsDouble();
+    }
 }
