d29ab3fb61a5a864d9cabbce2d49caec5092f02a
Wojciech Kurcab <wojciech@kurcab.com>
2019-05-05 11:45:11 +0200

    5-05

--- kodilla-stream/src/main/java/com/kodilla/stream/world/World.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/world/World.java
@@ -1,17 +1,17 @@
 package com.kodilla.stream.world;
 
+import java.math.BigDecimal;
 import java.util.HashSet;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 public final class World {
     private final Set<Continent> continents = new HashSet<>();
 
-    public Set<String> getPeopleQuantity() {
+    public BigDecimal getPeopleQuantity() {
         return continents.stream()
                 .flatMap(continent -> continent.getCountries().stream())
-                .map(Country::getCountryName)
-                .collect(Collectors.toSet());
+                .map(country -> country.getPeopleQuantity())
+                .reduce(BigDecimal.ZERO, (sum, current) -> sum = sum.add(current));
     }
 
     public void addContinent(Continent continent) {
