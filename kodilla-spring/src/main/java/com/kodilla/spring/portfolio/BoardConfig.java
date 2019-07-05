69909755670c9b043d0e76a4e7e64bedc43af07e
Wojciech Kurcab <wojciech@kurcab.com>
2019-07-05 18:27:03 +0200

    05-07

--- kodilla-spring/src/main/java/com/kodilla/spring/portfolio/BoardConfig.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/portfolio/BoardConfig.java
@@ -1,26 +1,43 @@
 package com.kodilla.spring.portfolio;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class BoardConfig {
-    @Bean
-    public Board board() {
-        return new Board(toDoList(), inProgressList(), doneList());
-    }
+    @Autowired
+    @Qualifier("toDoList")
+    public TaskList toDoList;
+    @Autowired
+    @Qualifier("inProgressList")
+    public TaskList inProgressList;
+    @Autowired
+    @Qualifier("doneList")
+    public TaskList doneList;
+
 
     @Bean
+    public Board board() {
+        return new Board(toDoList, inProgressList, doneList);
+    }
+
+    @Bean(name = "toDoList")
+    @Scope("prototype")
     public TaskList toDoList() {
         return new TaskList();
     }
 
-    @Bean
+    @Bean(name = "inProgressList")
+    @Scope("prototype")
     public TaskList inProgressList() {
         return new TaskList();
     }
 
-    @Bean
+    @Bean(name = "doneList")
+    @Scope("prototype")
     public TaskList doneList() {
         return new TaskList();
     }
