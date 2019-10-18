51c093d9357a52700a492004a05f5a4270797067
Wojciech Kurcab <rr151459@walstead.pl>
2019-10-18 15:58:58 +0200

    modul 24 update

--- kodilla-jdbc/src/test/java/com/kodilla/jdbc/DbManagerTestSuite.java
+++ kodilla-jdbc/src/test/java/com/kodilla/jdbc/DbManagerTestSuite.java
@@ -67,6 +67,6 @@ public class DbManagerTestSuite {
         }
         rs.close();
         statement.close();
-        Assert.assertEquals(1, counter);
+        Assert.assertEquals(2, counter);
     }
 }
