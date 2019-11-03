ac12630476e43183eb474c477c4985edfbd31e87
Wojciech Kurcab <wojciech@kurcab.com>
2019-11-03 17:23:08 +0100

    update

---
+++ kodilla-jdbc/src/test/java/com/kodilla/jdbc/StoredProcTestSuite.java
@@ -0,0 +1,20 @@
+package com.kodilla.jdbc;
+
+import org.junit.Test;
+
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class StoredProcTestSuite {
+    @Test
+    public void testUpdateVipLevels() throws SQLException {
+        //Given
+        DbManager dbManager = DbManager.getInstance();
+        String sqlUpdate = "UPDATE READERS SET VIP_LEVEL=\"Not set\"";
+        Statement statement = dbManager.getConnection().createStatement();
+        statement.executeUpdate(sqlUpdate);
+
+        //When
+
+    }
+}
